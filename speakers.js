2d3701b13d73f8198aa3a89c696175273b4161a6
manospits <mnls95@gmail.com>
2017-01-11 01:39:57 +0200

    fixed sort and added another field

--- speakers.js
+++ speakers.js
@@ -2,15 +2,59 @@
 
 function getSorted(selector, attrName) {
     return $($(selector).toArray().sort(function(a, b){
-        return $(a).text().toUpperCase().localeCompare($(b).text().toUpperCase());
+            var aVal = String(a.getAttribute(attrName)),
+            bVal = String(b.getAttribute(attrName));
+        return (aVal) < (bVal) ? 1 : -1;
    }));
 }
 
+var nmorder =0;
+
 $.fn.order_speakers = function() {
-    arr=getSorted('.conf_speaker','name');
+    arr=getSorted('.conf_speaker','data-name');
     $(".conf_speaker").remove();
-    $.each( arr, function( index, value ){
-        $("#speakers").append(value);
-    });
+    $("#famousorderbutton").html("Publicity");
+    if ( nmorder== 0 ) {
+        nmorder=1;
+        $("#nameorderbutton").html("Name &uarr;");
+        arr=arr.get().reverse();
+        var arrayLength = arr.length;
+        for (var i = arrayLength-1;i >=0; i--) {
+            $("#speakers").append(arr[i]);
+        }
+    } else {
+        nmorder=0;
+        $("#nameorderbutton").html("Name &darr;");
+        arr=arr.get().reverse();
+        var arrayLength = arr.length;
+        for (var i = 0; i <arrayLength; i++) {
+            $("#speakers").append(arr[i]);
+        }
+    }
+};
+var famorder=0;
+
+$.fn.order_speakers_famous = function() {
+    arr=getSorted('.conf_speaker','data-famous');
+    $("#nameorderbutton").html("Name");
+    $(".conf_speaker").remove();
+    nmorder=0;
+    if ( famorder== 0 ) {
+        famorder=1;
+        $("#famousorderbutton").html("Publicity &uarr;");
+        arr=arr.get().reverse();
+        var arrayLength = arr.length;
+        for (var i = arrayLength-1;i >=0; i--) {
+            $("#speakers").append(arr[i]);
+        }
+    } else {
+        famorder=0;
+        $("#famousorderbutton").html("Publicity &darr;");
+        arr=arr.get().reverse();
+        var arrayLength = arr.length;
+        for (var i = 0; i <arrayLength; i++) {
+            $("#speakers").append(arr[i]);
+        }
+    }
 };
 
