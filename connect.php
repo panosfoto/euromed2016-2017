e371bc6f2ca61c60765b732a5d093918a999e68e
manospits <mnls95@gmail.com>
2017-01-15 23:28:49 +0200

    final commit

--- connect.php
+++ connect.php
@@ -1,4 +1,4 @@
 <?php
- $db = mysqli_connect('localhost','root','NemesiS7512591!','Euromed')
+ $db = mysqli_connect('localhost','root','euromed','Euromed')
  or die('Error connecting to MySQL server.');
 ?>
